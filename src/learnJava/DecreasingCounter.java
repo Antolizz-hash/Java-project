8b82a8ba489e763d479591b3fb489aa34dc3aa63
Antolizz-hash <antonyfwende1@gmail.com>
2026-04-26 08:30:19 +0300

    Add file to learn manipulating counter

---
+++ src/learnJava/DecreasingCounter.java
@@ -0,0 +1,46 @@
+package learnJava;
+
+public class DecreasingCounter {
+	
+	private int value;
+	
+	public DecreasingCounter(int initialValue) {
+		this.value = initialValue;
+	}
+	
+	public void printValue() {
+		System.out.println("Value: " + this.value);
+	}
+	
+	public void decrement() {
+		if(this.value == 0) {
+//			System.out.println("Value: "+this.value);
+		}
+		else {
+			
+			this.value--;
+		}
+		
+	}
+	
+	public void reset() {
+		this.value = 0;
+	}
+	
+	public static void main(String[]args) {
+		
+		DecreasingCounter counter = new DecreasingCounter(100);
+		
+		counter.printValue();
+		
+		counter.reset();
+		counter.printValue();
+		
+		counter.decrement();
+		counter.printValue();
+		
+		
+	
+	}
+
+}
